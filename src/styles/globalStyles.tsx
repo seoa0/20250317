714bb29b9acb2d61114f86eb950b33c0d7a85f54
seoa0 <seoayoung12@naver.com>
2025-03-18 17:01:28 +0900

    fix font

--- src/styles/globalStyles.tsx
+++ src/styles/globalStyles.tsx
@@ -1,11 +1,10 @@
-import { Global, css } from "@emotion/react";
+import { css } from "@emotion/react";
 import { colors } from "@resources/colors";
 
 const styles = css`
   @font-face {
     font-family: "OngluipDahyun";
-    src: url("/fonts/OngluipDahyun.ttf") format("truetype"),
-      url("../public/fonts/OngluipDahyun.ttf") format("truetype");
+    src: url("/fonts/OngluipDahyun.ttf") format("truetype");
     font-weight: normal;
     font-style: normal;
     font-display: swap;
@@ -15,34 +14,48 @@ const styles = css`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  html {
+    font-size: 16px;
+
+    @media screen and (max-width: 768px) {
+      font-size: 14px;
+    }
+
+    @media screen and (max-width: 480px) {
+      font-size: 12px;
+    }
+  }
+
+  html,
+  body {
     font-family: "OngluipDahyun", sans-serif !important;
-    user-select: none;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
+  }
+
+  body {
+    background-color: ${colors.background.back_pink};
   }
 
   button {
     cursor: pointer;
-    background: none;
     border: none;
+    background: none;
+    font-size: 1rem;
     font-family: "OngluipDahyun", sans-serif !important;
   }
 
   a {
     text-decoration: none;
     color: inherit;
-    font-family: "OngluipDahyun", sans-serif !important;
-  }
-
-  body {
-    background-color: ${colors.background.back_pink};
+    font-size: 1rem;
     font-family: "OngluipDahyun", sans-serif !important;
   }
 
   input,
   textarea,
   select {
+    font-size: 1rem;
     font-family: "OngluipDahyun", sans-serif !important;
   }
 `;
